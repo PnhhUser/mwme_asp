2a3178e0507eba3427011d00c9373111c8dab63d
PnhhUser <pnhh240300@gmail.com>
2025-12-01 11:48:04 +0700

    Move file gitignore

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d584e8a19bd77f1796d91ba4e7038b306621447")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d421e4d6e7665c58da376d4b15882bff178db5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
